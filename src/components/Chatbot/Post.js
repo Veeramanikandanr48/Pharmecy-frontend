95048e9c31fc1b19cde2b751b5bd1d6f10eaf29e
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-04-18 10:08:49 +0530

    chat

--- src/components/Chatbot/Post.js
+++ src/components/Chatbot/Post.js
@@ -1,19 +1,20 @@
 import React, { useEffect } from 'react';
+import BACKEND_URL from '../../Data/config';
 import axios from 'axios';
 
 const Post = ({ steps }) => {
   useEffect(() => {
-    const { submit, firstname, lastname, email } = steps;
+    const { firstname, lastname, email } = steps;
     const userObject = {
-      submit: submit.value,
       first_name: firstname.value,
       last_name: lastname.value,
       email: email.value,
     };
+    console.log(userObject)
 
     const postData = async () => {
       try {
-        const res = await axios.post(`/api`, userObject);
+        const res = await axios.post(`${BACKEND_URL}chat`, userObject);
         console.log(res.status);
       } catch (error) {
         console.error(error);
